406441ce0f40bbf0a791393c2fd3c3aab795d328
DankGroundhog <felixperez78@hotmail.com>
2022-07-22 15:37:45 -0700

    Prints model without errors. Only needs logic error fixes, alot of them ':) Also comment cleaning, oops

--- model_maker.py
+++ model_maker.py
@@ -1,4 +1,5 @@
 import argparse
+from curses.ascii import isdigit
 from importlib.resources import open_binary
 import json
 import logging
@@ -20,6 +21,8 @@ import networkx as nx
 import onnx
 from onnx import ModelProto, helper, numpy_helper, onnx_pb, AttributeProto, TensorProto, GraphProto
 from onnx.helper import make_graph
+from _curses import *
+import re
 
 # def get_args():
 #     parser = argparse.ArgumentParser(description='onnxruntime modelmaking tool')
@@ -56,6 +59,7 @@ def process_and_make():
     input_buffer, output_buffer, op_buffer, attrib_buffer = None, None, None, None
     input_pos = 0 # This var is used for the input/output tags of the nodes
     initial_input = None
+    
 
     for i in range(len(data_df)):
         buffer_string = ''
@@ -76,42 +80,75 @@ def process_and_make():
             else:
                 buffer_list = attrib_buffer.strip('[]').split(',')
                 for i in range(len(buffer_list)):
-                    buffer_list[i] = int(buffer_list[i].strip().strip('\''))
+                    if type(buffer_list[i]) == type(str()) and re.search('[a-zA-Z]+', buffer_list[i]):
+                            buffer_list[i] = str(buffer_list[i].strip(" [\' \'] "))
+                    else:
+                        buffer_list[i] = int(buffer_list[i].strip(" \' [\' \'] \'"))
+                    #buffer_list[i] = int(buffer_list[i].strip().strip('\''))
 
             input_buffer = ''.join(str(list(input_buffer[0].values())))
             input_buffer = input_buffer.strip('[]').split(',')
-            for item in range(len(input_buffer)): input_buffer[item] = int(input_buffer[item])
+            if input_buffer[0] != '':
+                for item in range(len(input_buffer)): input_buffer[item] = int(input_buffer[item])
             output_buffer = ''.join(str(list(output_buffer[0].values())))
             output_buffer = output_buffer.strip('[]').split(',')
-            for item in range(len(output_buffer)): output_buffer[item] = int(output_buffer[item])
+            if output_buffer[0] != '':
+                for item in range(len(output_buffer)): output_buffer[item] = int(output_buffer[item])
 
         elif list(input_buffer[0].keys())[0] == 'float':
             input_buffer = ''.join(str(list(input_buffer[0].values())))
             input_buffer = input_buffer.strip('[]').split(',')
-            for item in range(len(input_buffer)): input_buffer[item] = float(input_buffer[item])
+            if input_buffer[0] != '':
+                for item in range(len(input_buffer)): input_buffer[item] = float(input_buffer[item])
             output_buffer = ''.join(str(list(output_buffer[0].values())))
             output_buffer = output_buffer.strip('[]').split(',')
-            for item in range(len(output_buffer)): output_buffer[item] = float(output_buffer[item])
+
+            if output_buffer[0] != '':
+                for item in range(len(output_buffer)): output_buffer[item] = float(output_buffer[item])
 
             if attrib_buffer == '[]':
                 buffer_list = []
             else:
                 if attrib_buffer.find("[[[") == -1:
                     # I know this looks super redundant, but this is how it works consistently trust me
-                    attrib_list_dump = []
-                    buffer_list = attrib_buffer.strip('[ \' [\' \'] \' ]').split(',')
-                    for i in range(0, len(buffer_list), 2):
-                        buffer_list[i] = str(buffer_list[i].strip(" [\' \'] "))
-                        if i != len(buffer_list):
-                            buffer_list[i+1] = float(buffer_list[i+1].strip(" \' [\' \'] \'"))
-                    for i in range(0, len(buffer_list), 2):
-                        if i != len(buffer_list):
-                            buffer_list[i] = [buffer_list[i], buffer_list[i+1]]        
-                    for j in range(len(buffer_list)):
-                        if isinstance(buffer_list[j], list):
-                            attrib_list_dump.append(buffer_list[j])
+                    attrib_list_dump, name, attr_vals = [], None, []
+                    buffer_list = attrib_buffer.strip(" [\' \'] ").strip('[ \' [\' \'] \' ]').split(',')
+                    for i in range(len(buffer_list)):
+                        # If it is not a digit, string, else, corresponding datatype
+                        if type(buffer_list[i]) == type(str()) and re.search('[a-zA-Z]+', buffer_list[i]):
+                            buffer_list[i] = str(buffer_list[i].strip(" [\' \'] "))
+                        else:
+                        # if i != len(buffer_list):
+                            # buffer_list[i] = buffer_list[i].strip(" \' [\' \'] \'")
+                            buffer_list[i] = float(buffer_list[i].strip(" \' [\' \'] \'"))
+                    # if it is a string, assign to name, else, assign to attr_vals. Then merge again in attrib_list
+                    i = 0
+                    while i != (len(buffer_list)):
+                        if type(buffer_list[i]) == type(str()):
+                            name = buffer_list[i]
+                        else:
+                            if not (i >= len(buffer_list) - 1) and type(buffer_list[i+1]) != type(str()):
+                                attr_vals.append(buffer_list[i])
+                            else:
+                                attr_vals.append(buffer_list[i])
+                                attrib_list_dump.append([name, attr_vals])
+                                attr_vals = []
+                        i += 1
+                    # switch = True
+                    # for i in range(len(buffer_list)):
+                    #     if type(buffer_list[i]) == type(str()) and switch:
+                    #         name = buffer_list[i]
+                    #         switch = False
+                    #     if switch == False:
+                    #         if (type(buffer_list[i]) == type(str())):
+                    #             switch = True
+                    #         attr_vals.append(buffer_list[i])
+                    # ------------------------------------------------------------------------    
+                    # for j in range(len(buffer_list)):
+                    #     if isinstance(buffer_list[j], list):
+                    #         attrib_list_dump.append(buffer_list[j])
                     buffer_list = attrib_list_dump
-                    del attrib_list_dump
+                    del attrib_list_dump, name
                 else:
                     buffer_list = attrib_buffer.strip('[]').split(',')
                     buffer_list[0] = str(buffer_list[0].strip("\'"))                   
@@ -126,9 +163,9 @@ def process_and_make():
 
         attrib_buffer = buffer_list
         del buffer_list
-        
+
         # attrib_buffer = dict(attrib_buffer)
-        op_buffer = data_df['op_type'][i]
+        op_buffer = data_df['op_type'][input_pos]
 
         # Fix input encoding: Passes 1 string - Model interprets as a list of
         # Convert the node list to a directed graph, then turn that graph into the model?
@@ -146,8 +183,14 @@ def process_and_make():
         #kernel shape = attrib
         #input = list of names
         #input name is just a 'pointer'
-        #op_buffer + pos number for the input 
-        nodes.append(make_node(op_buffer, inputs=str(input_pos), outputs=str(input_pos + 1), kernel_shape=input_buffer, kwargs=attrib_buffer))
+        #op_buffer + pos number for the input
+        #   
+        
+        #Adding kernel_hape to the attribute list
+        attrib_buffer = [["attributes", attrib_buffer], ["kernel_shape", input_buffer]]
+        attrib_buffer = dict(attrib_buffer)
+        # nodes.append(make_node(op_buffer, inputs=str(input_pos), outputs=str(input_pos + 1), name=str(input_pos), kernel_shape=input_buffer, kwargs=attrib_buffer))
+        nodes.append(make_node(op_buffer, inputs=str(input_pos), outputs=str(input_pos + 1), name=str(input_pos), kwargs=attrib_buffer))
         input_pos += 1
         if i == 0: initial_input = input_buffer
         input_buffer, output_buffer, op_buffer, attrib_buffer = None, None, None, None
