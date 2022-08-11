50e4352a79056263438896f455c5b0b0f5eacb18
DankGroundhog <felixperez78@hotmail.com>
2022-08-11 15:51:26 -0700

    Tried using onnx surgeon module to topologically sort graph. Issues ensued.

--- model_maker.py
+++ model_maker.py
@@ -17,9 +17,8 @@ import json
 import os
 import csv
 
-import tf2onnx
-
-import networkx as nx
+import onnx_graphsurgeon as gs # Must download (python[3] -m pip install onnx_graphsurgeon --index-url https://pypi.ngc.nvidia.com)
+# python[3] -m pip install colored (This one is also needed)
 
 import onnx
 from onnx import ModelProto, helper, numpy_helper, onnx_pb, AttributeProto, TensorProto, GraphProto
@@ -117,33 +116,6 @@ def process_and_make(main_dir):
                 # input_buffer, output_buffer, op_buffer, attrib_buffer = None, None, None, None
                 break
 
-
-        # # Code segment to find the first op that fits one graph input
-        # # Actually do it recursively, might be easier than expected
-        # count = 0
-        # for op in range(len(data_df)):
-        #     op_buffer = data_df['op_type'][op]
-            
-        #     i_o = data_df["I/O"][op].split(',')
-        #     input = str(i_o[0].strip("\' [] \'"))
-        #     output = str(i_o[1].strip("\' [] \'"))
-        #     if op == 0:
-        #         for node in range(len(data_df)):
-        #             i_o = data_df["I/O"][node].split(',')
-        #             input = str(i_o[0].strip("\' [] \'"))
-        #             output = str(i_o[1].strip("\' [] \'"))
-        #             for item in range(len(graph_input_buffer)):
-        #                 if input == graph_input_buffer[item].name:
-        #                     nodes.append(make_node(op_buffer, inputs=[input], outputs=[output], name=data_df["name"][i], attributes=data_df["attribute"][i]))
-        #                     count += 1
-        #     # Could have more than one op consuming the graph inputs jumps to the next node
-        #     # after the ones already in the node list
-        #     j = i + count
-        #     for j in range(len(nodes)):
-        #         prev_out = nodes[j-1].output
-        #         if prev_out[0] == input:
-        #             nodes.append(make_node(op_buffer, inputs=[input], outputs=[output], name=data_df["name"][j], attributes=data_df["attribute"][j]))
-
         for i in range(len(data_df)):
             op_buffer = data_df['op_type'][i]
             
@@ -154,37 +126,16 @@ def process_and_make(main_dir):
             # attr_name = list(attribute_obj.keys())[0]
             nodes.append(make_node(op_buffer, inputs=[input], outputs=[output], name=data_df["name"][i], **json.loads(data_df["attribute"][i].replace('\'', '\"'))))
             
-            # New Code segment with arrangement logic down here
-            # i_o = data_df["I/O"][i].split(',')
-            # input = str(i_o[0].strip("\' [] \'"))
-            # output = str(i_o[1].strip("\' [] \'"))
-            # if i == 0:
-            #     j = i
-            #     for j in range(len(graph_input_buffer)):
-            #         i_o = data_df["I/O"][i].split(',')
-            #         input = str(i_o[0].strip("\' [] \'"))
-            #         output = str(i_o[1].strip("\' [] \'"))
-            #         if input == graph_input_buffer[j]:
-            #             nodes.append(make_node(op_buffer, inputs=[input], outputs=[output], name=data_df["name"][i], attributes=data_df["attribute"][i]))
-    
-    # nodes = tf2onnx.graph_builder
-    os.chdir(f'..')
-
-    model = helper.make_model(
-    opset_imports=[helper.make_operatorsetid('', 11)],
-    producer_name='Synthetic-Model-test',
-    graph=make_graph(
-        name='test',
-        # inputs=[helper.make_tensor_value_info('A', TensorProto.FLOAT, shape=initial_input)],
-        inputs=graph_input_buffer,
-        outputs=graph_output_buffer,
-        # outputs=[helper.make_tensor_value_info('Y', TensorProto.FLOAT, shape=[1,25])],
-        nodes=nodes,
-        ),
-    )
-    # model.graph.topological_sort(nodes)
     onnx.save(model, os.path.join(os.getcwd(), "synthetic_model.onnx"))
+    model = gs.import_onnx(onnx.load("synthetic_model.onnx"))
+    os.remove("synthetic_model.onnx")
+    model = model.toposort()
+    # os.remove("synthetic_model.onnx")
+    onnx.save(gs.export_onnx(model), "synthetic_model.onnx")
     print(f"Output in {os.getcwd()}")
+
+    # graph = Graph(len(nodes))
+    # graph.graph = model.graph
 
 if __name__ == "__main__":
     process_and_make()
